3c899720eac9b1a2d3c25e65cbfb6b28556d51fb
Mahdyar <m4hdyar@gmail.com>
2021-12-02 13:45:35 +0100

    Initial commit: Project created with hello world and a simple test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    public static int echoInt(int input){
+        return input;
+    }
+}
