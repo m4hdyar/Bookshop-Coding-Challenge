a9a648c599b5e1042516810544c4312f39800ffa
Mahdyar <m4hdyar@gmail.com>
2021-12-12 19:47:17 +0100

    Add equal check function to Stock EntityComparePair

--- src/main/java/mainpack/shop/StockEntityComparePair.java
+++ src/main/java/mainpack/shop/StockEntityComparePair.java
@@ -1,5 +1,7 @@
 package mainpack.shop;
 
+import java.util.Objects;
+
 public class StockEntityComparePair {
     public final StockEntity firstStockEntity;
     public final StockEntity secondStockEntity;
@@ -9,4 +11,17 @@ public class StockEntityComparePair {
         this.secondStockEntity = secondStockEntity;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StockEntityComparePair that = (StockEntityComparePair) o;
+        return (Objects.equals(firstStockEntity, that.firstStockEntity) && Objects.equals(secondStockEntity, that.secondStockEntity)) ||
+                (Objects.equals(firstStockEntity, that.secondStockEntity) && Objects.equals(secondStockEntity, that.firstStockEntity)) ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstStockEntity, secondStockEntity);
+    }
 }
